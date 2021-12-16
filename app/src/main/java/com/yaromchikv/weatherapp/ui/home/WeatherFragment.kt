f97a2b32f925390492f4b83924921b4a84d645ca
Vladislav Yaromchik <vladislav.yaromchik@gmail.com>
2021-12-16 15:54:29 +0300

    feat: change fragments

--- app/src/main/java/com/yaromchikv/weatherapp/ui/home/WeatherFragment.kt
+++ app/src/main/java/com/yaromchikv/weatherapp/ui/home/WeatherFragment.kt
@@ -1,4 +1,4 @@
-package com.yaromchikv.weatherapp.ui.dashboard
+package com.yaromchikv.weatherapp.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.yaromchikv.weatherapp.R
-import com.yaromchikv.weatherapp.databinding.FragmentDashboardBinding
+import com.yaromchikv.weatherapp.databinding.FragmentHomeBinding
 
-class DashboardFragment : Fragment() {
+class WeatherFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var weatherViewModel: WeatherViewModel
+    private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        weatherViewModel =
+            ViewModelProvider(this).get(WeatherViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textHome
+        weatherViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
